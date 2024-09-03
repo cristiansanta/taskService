a196d56ddcb733e65753de9980cd1b33b848cd1e
CristianAC <cristiansanta5@gmail.com>
2024-09-03 17:05:10 -0500

    schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tasks (
-                                     id SERIAL PRIMARY KEY,
-                                     title VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
     description TEXT,
     status VARCHAR(50) NOT NULL,
-    user_id BIGINT
+    user_email VARCHAR(80)
     );
